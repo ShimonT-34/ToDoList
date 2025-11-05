3ff5bbb2625dc36d41c83f80021ef18cc54e3a44
Talleres Puente Alto <tallerespa@inacapmail.cl>
2025-11-05 18:15:27 -0300

    Primer commit

---
+++ ToDoListApp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Tarea(models.Model):
+    titulo = models.CharField(max_length=200)
+    descripcion = models.TextField(blank=True)
+    completado = models.BooleanField(default=False)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
